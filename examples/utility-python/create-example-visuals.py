0223dbba07c8cdf7edb1113885aaf20c948227da
Martin Chan <martinchan53@gmail.com>
2023-10-24 15:25:52 +0100

    feat: set up py example script and tidy headers

---
+++ examples/utility-python/create-example-visuals.py
@@ -0,0 +1,7 @@
+"""
+Viva Insights - Python Utility Script
+================================
+This script provides a demo on how to generate example visuals using the 'vivainsights' Python library
+All outputs are saved as SVG in a local output folder that you can customize the path to. 
+"""
+
--- examples/utility-r/create-example-visuals.R
+++ examples/utility-r/create-example-visuals.R
@@ -1,4 +1,8 @@
+#' ==================================================================================================
+#' Viva Insights - R Utility Script
 #' This script provides a demo on how to generate example visuals using the 'vivainsights' R library
+#' All outputs are saved as SVG in a local output folder that you can customize the path to. 
+#' ==================================================================================================
 
 # Set up -------------------------------------------------------------------------------------------
 # Load packages
